e309d6c19583beef3d3969a55902b1795dc1564d
hjq20021984 <hjq20021984@163.com>
2014-08-16 15:46:34 +0800

    add systemintro

--- app/controllers/gatheringbill/gatheringBillModel.js
+++ app/controllers/gatheringbill/gatheringBillModel.js
@@ -63,20 +63,38 @@ var modelExtraInfo = {
 
 function main(Y) {
 	if (g_id) {
-		ajaxRequest({
-			url : "/" + g_dataSourceJson.Id + "/GetData?format=json",
-			params : {
-				"dataSourceModelId" : g_dataSourceJson.Id,
-				"formTemplateId" : g_formTemplateJsonData.Id,
-				"id" : g_id
-			},
-			callback : function(o) {
-				var formManager = new FormManager();
-				formManager.applyGlobalParamFromAjaxData(o);
-				formManager.loadData2Form(g_dataSourceJson, o.bo);
-				formManager.setFormStatus(g_formStatus);
-			}
-		});
+		if (g_copyFlag == "true") {// 复制
+			ajaxRequest({
+				url: "/" + g_dataSourceJson.Id + "/CopyData?format=json"
+				,params: {
+					"dataSourceModelId": g_dataSourceJson.Id,
+					"formTemplateId": g_formTemplateJsonData.Id,
+					"id": g_id
+				},
+				callback: function(o) {
+					var formManager = new FormManager();
+					formManager.setDetailIncId(g_dataSourceJson, o.bo);
+					formManager.applyGlobalParamFromAjaxData(o);
+					formManager.loadData2Form(g_dataSourceJson, o.bo);
+					formManager.setFormStatus("edit");
+				}
+			});
+		} else {
+			ajaxRequest({
+				url : "/" + g_dataSourceJson.Id + "/GetData?format=json",
+				params : {
+					"dataSourceModelId" : g_dataSourceJson.Id,
+					"formTemplateId" : g_formTemplateJsonData.Id,
+					"id" : g_id
+				},
+				callback : function(o) {
+					var formManager = new FormManager();
+					formManager.applyGlobalParamFromAjaxData(o);
+					formManager.loadData2Form(g_dataSourceJson, o.bo);
+					formManager.setFormStatus(g_formStatus);
+				}
+			});
+		}
 	} else {
 		ajaxRequest({
 			url : "/" + g_dataSourceJson.Id + "/NewData?format=json",
